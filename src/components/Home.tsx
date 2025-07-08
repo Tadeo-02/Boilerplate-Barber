d793d3d7a961afe56efb2cc8c85608dc5b3f586b
Tadeo-02 <153774639+Tadeo-02@users.noreply.github.com>
2025-07-08 20:53:35 -0300

    Agregado de iconos a los botones de secciones Home y Header; añadido de desplazamiento de Sidebar (basico), cambio de icono

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,14 +1,19 @@
 import styles from './home.module.css';
+import {FaShoppingCart} from 'react-icons/fa';
+import { PiScissorsFill } from "react-icons/pi";
+
 
 function Home() {
   return (
-    <div className= {styles.homeContainer}>
-        <button className={styles.button}>
-          SACAR TURNO
-        </button>
-        <button className={styles.button}>
-          COMPRAR PRODUCTOS
-        </button>
+    <div className={styles.homeContainer}>
+      <button className={styles.button}>
+        <PiScissorsFill style={{ fontSize: "4rem" }} />
+        SACAR TURNO
+      </button>
+      <button className={styles.button}>
+        <FaShoppingCart style={{ fontSize: "4rem" }} />
+        COMPRAR PRODUCTOS
+      </button>
     </div>
   );
 }
